927069a1a9229429489838cffa0826593ee2a7bb
Swaminathan11 <swaminathan.s@fivetran.com>
2023-04-07 00:28:55 +0530

    Testing rebase

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app = express()
 const mongoose = require('mongoose')
-const PORT_NAME = process.env.PORT||5000
+const POST = process.env.PORT||5000
 const {MONGOURI} = require('./config/keys')
 
 //mA18CAARr3u5hVNC
@@ -35,4 +35,4 @@ if(process.env.NODE_ENV=="production"){               //if deployed then serve s
     })
 }                    
 
-app.listen(PORT_NAME)
+app.listen(POST)
